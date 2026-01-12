0645e2f529a2317ef38efc3dce7f3ede012b4cc2
Andrius Alimas <alimaswebdeveloper@gmail.com>
2026-01-12 17:51:25 +0000

    Added first controller for testing root home page

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController
+{
+    #[Route('/', name: 'home')]
+    public function homepage(){
+       return new Response('Title: Welcome to Symfony!!!');
+    }
+}
