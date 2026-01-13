4dd9f35b021e03191ac9cf6ab6271ef921d2ffc6
Andrius Alimas <alimaswebdeveloper@gmail.com>
2026-01-13 12:24:20 +0000

    fix error with controller using Route and namespace

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -1,9 +1,13 @@
 <?php
 
+namespace App\Controller;
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PlaceController 
 {
+    #[Route('/','name: homepage')]
     public function homepage() : Response
     {
         return new Response('Title: Places');
